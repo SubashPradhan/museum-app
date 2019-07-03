4ae9005771a523628ebf13043e40cfc0fab92b2f
Subash Pradahan <suprowilde5@gmail.com>
2019-07-03 10:47:03 +0200

    display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,18 +1,22 @@
 function submitComment(){
-    const inptField = document.getElementById('name');
-    const name = inptField.value; 
+    const inputField = document.getElementById('name');
+    const name = inputField.value; 
     const textArea = document.getElementById('msg');
-    const msg = inptField.value;
+    const msg = inputField.value;
     
-    const section = document.createElement('section');
+    const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
 
-    h3.innerHTML = '${name] said:'
+    h3.innerHTML = `${name} said:`;
     p.innerHTML = msg;
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
 
-    console.log(comment);
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
+
+    inputField.value = null;
+    textArea.value = null;
 }

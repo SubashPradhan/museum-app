95061dcbd715a448277db70bf19d79952e07ccc1
Subash Pradahan <suprowilde5@gmail.com>
2019-07-03 10:21:12 +0200

    Create Elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,18 @@
 function submitComment(){
-    console.log('Hello World');
+    const inptField = document.getElementById('name');
+    const name = inptField.value; 
+    const textArea = document.getElementById('msg');
+    const msg = inptField.value;
+    
+    const section = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    h3.innerHTML = '${name] said:'
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+
+    console.log(comment);
 }
